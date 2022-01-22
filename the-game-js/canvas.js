5bee54b8c6746e3cdaeaed018c93ba307732db09
Tim Langford <tim.langford@gmail.com>
2022-01-22 23:19:52 +0000

    Added basic line intersetcting ray tracer.

--- the-game-js/canvas.js
+++ the-game-js/canvas.js
@@ -37,7 +37,7 @@ class PlayerEntity {
         this.rot = 0;
         this.dim = dimension;
         this.vel = velocity;
-        this.speed = 10;
+        this.speed = 5;
         // Systems
         this.screenEdgeHandler = screenEdgeHandler;
         this.renderer = renderer;
@@ -109,6 +109,14 @@ function drawCircle(pos, radius, color) {
     ctx.arc(pos.x, pos.y, radius, 0, Math.PI * 2);
     ctx.fillStyle = color;
     ctx.fill();
+}
+
+function drawLine(p1, p2, color) {
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.strokeStyle = color;
+    ctx.stroke();
 }
 
 class TileCircleRenderSystem {
@@ -204,15 +212,11 @@ class OrientationMovementSystem {
     moveForward() {
         this.entity.vel.dx = Math.cos(this.entity.rot) * this.entity.speed;
         this.entity.vel.dy = Math.sin(this.entity.rot) * this.entity.speed;
-        // this.entity.pos.dx = this.entity.vel.dx;
-        // this.entity.pos.dy = this.entity.vel.dy;
     }
 
     moveBackward() {
         this.entity.vel.dx = -Math.cos(this.entity.rot) * this.entity.speed;
         this.entity.vel.dy = -Math.sin(this.entity.rot) * this.entity.speed;
-        // this.entity.pos.dx = -this.entity.vel.dx;
-        // this.entity.pos.dy = -this.entity.vel.dy;
     }
 
     rotateRight() {
@@ -347,6 +351,83 @@ class TileMapRenderSystem {
     }
 }
 
+// Edge Entity --------------------------------------------------------------
+//
+
+class Point2DComponent {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class EdgeEntity {
+    constructor(start, end, renderer) {
+        this.start = start;
+        this.end = end;
+        this.renderer = renderer;
+    }
+
+    handle() {
+        this.renderer.handle(this);
+    }
+}
+
+class EdgeRenderSystem {
+    constructor(ctx) {
+        this.ctx = ctx;
+    }
+
+    handle(entity) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(entity.start.x, entity.start.y);
+        this.ctx.lineTo(entity.end.x, entity.end.y);
+        this.ctx.strokeStyle = 'white';
+        this.ctx.stroke();
+    }
+}
+
+// Ray ----------------------------------------------------------------------
+//
+//
+// Props: https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+//        https://www.youtube.com/watch?v=TOEi6T2mtHo
+//
+//
+function raycast(ray, edge) {
+    const { x: x1, y: y1 } = edge.start;
+    const { x: x2, y: y2 } = edge.end;
+    const { x: x3, y: y3 } = ray.pos;
+    const { x: x4, y: y4 } = ray.dir;
+
+    // Denominator.
+    const d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+    if (d === 0) {
+        // parallel lines.
+        return undefined;
+    } else {
+        const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / d;
+        const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / d;
+        // Only consider 'u > 0' as the ray does not extend backwards.
+        if (t > 0 && t < 1 && u > 0) {
+            // Use 'p point' solution.
+            const x = x1 + t * (x2 - x1);
+            const y = y1 + t * (y2 - y1);
+            return { x, y };
+        }
+    }
+    return undefined;
+}
+
+// function raycast(source, destination) {
+//     const dx = destination.x - source.x;
+//     const dy = destination.y - source.y;
+//     const angle = Math.atan2(dy, dx); // NB: 'y' comes first for 'atan2'!
+//     const velocity = new Vec2D(Math.cos(angle), Math.sin(angle));
+//     return velocity;
+// }
+
 // Initialise -----------------------------------------------------------------
 //
 
@@ -368,7 +449,7 @@ const player = new PlayerEntity(
 );
 
 // const inputHandler = new CompassMovementSystem(player);
-const inputHandler = new OrientationMovementSystem(player);
+const playerMovementHandler = new OrientationMovementSystem(player);
 
 // Map
 //
@@ -387,6 +468,102 @@ const map01 = [
 
 const map = new TileMapEntity(new TileMapComponent(8, 8, 32, 32, map01), new TileMapRenderSystem(ctx));
 
+// Ray
+//
+class Raycaster {
+    constructor(pos, dir, renderer) {
+        this.pos = pos;
+        this.dir = dir;
+        this.renderer = renderer;
+    }
+
+    handle() {
+        this.renderer.handle(this);
+    }
+
+    lookAt(x, y) {
+        // TODO: Normalise to Unit vector.
+        this.dir = { x, y };
+    }
+}
+
+class RaycasterRenderSystem {
+    constructor(ctx, radius, color) {
+        this.ctx = ctx;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    handle(entity) {
+        drawCircle(entity.pos, this.radius, this.color);
+        drawLine(entity.pos, entity.dir, 'green');
+    }
+}
+
+// Edges
+//
+const edgeRenderer = new EdgeRenderSystem(ctx);
+
+const randomPoint = () => {
+    return {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+    };
+};
+
+const edges = [];
+const addBorderEdges = true;
+if (addBorderEdges) {
+    const offset = 10;
+    const p1 = { x: offset, y: offset };
+    const p2 = { x: canvas.width - offset, y: offset };
+    const p3 = { x: canvas.width - offset, y: canvas.height - offset };
+    const p4 = { x: offset, y: canvas.height - offset };
+
+    const e1 = new EdgeEntity(p1, p2, edgeRenderer);
+    const e2 = new EdgeEntity(p2, p3, edgeRenderer);
+    const e3 = new EdgeEntity(p3, p4, edgeRenderer);
+    const e4 = new EdgeEntity(p4, p1, edgeRenderer);
+
+    edges.push(e1, e2, e3, e4);
+}
+
+const addRandomEdges = true;
+if (addRandomEdges) {
+    const e1 = new EdgeEntity(randomPoint(), randomPoint(), edgeRenderer);
+    const e2 = new EdgeEntity(randomPoint(), randomPoint(), edgeRenderer);
+    const e3 = new EdgeEntity(randomPoint(), randomPoint(), edgeRenderer);
+    const e4 = new EdgeEntity(randomPoint(), randomPoint(), edgeRenderer);
+
+    edges.push(e1, e2, e3, e4);
+}
+
+const raycasterRenderer = new RaycasterRenderSystem(ctx, 3, 'green');
+const raycaster = new Raycaster(
+    { x: canvas.width / 2, y: canvas.height / 2 },
+    { x: canvas.width / 2 + 10, y: canvas.height / 2 },
+    raycasterRenderer
+);
+
+class RayCasterMouseLookAtSystem {
+    constructor(entity) {
+        this.entity = entity;
+        canvas.addEventListener('mousemove', (e) => {
+            let mpos = getMousePos(canvas, e);
+            raycaster.lookAt(mpos.x, mpos.y);
+        });
+    }
+}
+const rayMovementHandler = new RayCasterMouseLookAtSystem(raycaster);
+
+const getMousePos = (canvas, evt) => {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top,
+    };
+};
+
 // Game Loop ------------------------------------------------------------------
 //
 
@@ -396,6 +573,20 @@ function gloop() {
     map.handle();
     player.handle();
 
+    // edge01.handle();
+
+    raycaster.handle();
+    for (let i = 0; i < edges.length; i++) {
+        edges[i].handle();
+        const hit = raycast(raycaster, edges[i]);
+        if (hit) {
+            drawCircle(hit, 3, 'pink');
+        }
+    }
+
+    // drawLine(raycaster.pos, raycaster.dir, 'green');
+    // const i = intersect(ray, edge);
+    // console.log(`Intersect ${i}`);
     requestAnimationFrame(gloop);
 }
 gloop();
